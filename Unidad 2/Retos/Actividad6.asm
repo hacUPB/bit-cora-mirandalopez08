c8e60050cbb68292eb23758a2ccc17ac11671e6a
mirandalopez08 <lopezmiranda0608@gmail.com>
2026-02-09 11:12:38 -0500

    act 6 int 2

--- Unidad 2/Retos/Actividad6.asm
+++ Unidad 2/Retos/Actividad6.asm
@@ -1,17 +1,31 @@
+@2
+M=0        // iniciamos la casilla 12 en 0 para asegurarnos de que el valor sea 0
+
 @1
 D=A        // D = 1
 @2000
-M=D   
+M=D        // en la casilla 13 se guarda el valor 1, esta casilla sera como un contador
 
-(LOOP)
+(LOOP)     // inicio del ciclo
 
 @2000
-D=M        
-@10
-D=D+A      
+D=M        // D = i
+@11
+D=D-A      // D = i - 6
 @END
-D;JGE      
+D;JGE      // Si i >= 6, salta a END (termina el ciclo)
 
 @2000
-D=M        
+D=M        // D = i
 @2
+M=D+M     // suma = suma + i
+
+@2000
+M=M+1     // i = i + 1
+
+@LOOP
+0;JMP      // Salta incondicionalmente a LOOP
+
+(END)      // Etiqueta de fin del programa
+@END
+0;JMP      // Bucle infinito para detener la ejecución
