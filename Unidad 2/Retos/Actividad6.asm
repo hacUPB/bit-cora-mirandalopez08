655efc5ee33bd4c6ea23f187099b61b565723c9d
mirandalopez08 <lopezmiranda0608@gmail.com>
2026-02-13 15:32:03 -0500

    end of act 6

--- Unidad 2/Retos/Actividad6.asm
+++ Unidad 2/Retos/Actividad6.asm
@@ -1,43 +1,103 @@
 @100
-D=A 
-@ARR 
-M=D 
-
-@j
-M=1
-
-@sum
-M = 0
-
-(ARREGLO)
-@j 
-D=M
-
+D=A
 @ARR
-A=M
-M=D
-D=D+1
-
-@j 
 M=D
 
+// arr[0] = 11
 @11
-D=D-A 
+D=A
+@100
+M=D
+
+// arr[1] = 233
+@233
+D=A
+@101
+M=D
+
+// arr[2] = 23
+@23
+D=A
+@102
+M=D
+
+// arr[3] = 77
+@77
+D=A
+@103
+M=D
+
+// arr[4] = 112
+@112
+D=A
+@104
+M=D
+
+// arr[5] = 61
+@61
+D=A
+@105
+M=D
+
+// arr[6] = 67
+@67
+D=A
+@106
+M=D
+
+// arr[7] = 98
+@98
+D=A
+@107
+M=D
+
+// arr[8] = 900
+@900
+D=A
+@108
+M=D
+
+// arr[9] = 810
+@810
+D=A
+@109
+M=D
+
+
+// sum = 0
+@sum
+M=0
+
+// j = 0
+@j
+M=0
+
+(LOOP)
+
+// if j >= 10 goto END
+@j
+D=M
+@10
+D=D-A
 @END
 D;JGE
 
-@ARR 
-D=M
-D=D+1
-M=D
-@ARREGLO 
+
+@ARR
+D=M        
+@j
+A=D+M      
+D=M       
+
+@sum
+M=M+D
+
+@j
+M=M+1
+
+@LOOP
 0;JMP
 
-(LOOP)
-@ARR
-D=M
-@sum
-M=D-1
-
-@100
-D;JLT
+(END)
+@END
+0;JMP
