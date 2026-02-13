e4b27e10887752f9535a4fbf61b7e151e4dcc466
mirandalopez08 <lopezmiranda0608@gmail.com>
2026-02-13 15:24:13 -0500

    delete second part of act 5 to put it in act5_2

--- Unidad 2/Investigacion/Actividad5.asm
+++ Unidad 2/Investigacion/Actividad5.asm
@@ -22,35 +22,3 @@ M=D
 (END)
 @END
 0;JMP
-
-// Programa 2
-
-// a = 10
-@10
-D=A
-@a
-M=D
-
-// b = 5
-@5
-D=A
-@b
-M=D
-
-// p = &a
-@a
-D=A
-@p
-M=D
-
-// b = *p
-@p
-A=M
-D=M
-@b
-M=D
-
-// Fin del programa
-(END)
-@END
-0;JMP
