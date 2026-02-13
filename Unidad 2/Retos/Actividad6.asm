6924811ca1e8442131fac4d1f3794f83a1c8e1f6
mirandalopez08 <lopezmiranda0608@gmail.com>
2026-02-13 10:34:45 -0500

    act 6 flata completar

--- Unidad 2/Retos/Actividad6.asm
+++ Unidad 2/Retos/Actividad6.asm
@@ -1,31 +1,43 @@
-@2
-M=0        // iniciamos la casilla 12 en 0 para asegurarnos de que el valor sea 0
+@100
+D=A 
+@ARR 
+M=D 
 
-@1
-D=A        // D = 1
-@2000
-M=D        // en la casilla 13 se guarda el valor 1, esta casilla sera como un contador
+@j
+M=1
 
-(LOOP)     // inicio del ciclo
+@sum
+M = 0
 
-@2000
-D=M        // D = i
+(ARREGLO)
+@j 
+D=M
+
+@ARR
+A=M
+M=D
+D=D+1
+
+@j 
+M=D
+
 @11
-D=D-A      // D = i - 6
+D=D-A 
 @END
-D;JGE      // Si i >= 6, salta a END (termina el ciclo)
+D;JGE
 
-@2000
-D=M        // D = i
-@2
-M=D+M     // suma = suma + i
+@ARR 
+D=M
+D=D+1
+M=D
+@ARREGLO 
+0;JMP
 
-@2000
-M=M+1     // i = i + 1
+(LOOP)
+@ARR
+D=M
+@sum
+M=D-1
 
-@LOOP
-0;JMP      // Salta incondicionalmente a LOOP
-
-(END)      // Etiqueta de fin del programa
-@END
-0;JMP      // Bucle infinito para detener la ejecución
+@100
+D;JLT
